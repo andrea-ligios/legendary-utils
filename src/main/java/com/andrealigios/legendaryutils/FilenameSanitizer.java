aba0ee151e08a3e8bd833d5380833f08169c4b95
Andrea Ligios <andrea.ligios@gmail.com>
2018-03-09 17:54:52 +0100

    Initial Commit

--- src/main/java/com/andrealigios/legendaryutils/FilenameSanitizer.java
+++ src/main/java/com/andrealigios/legendaryutils/FilenameSanitizer.java
@@ -1,41 +1,207 @@
 package com.andrealigios.legendaryutils;
 
-import java.util.Date;
 import java.util.Objects;
 import java.util.Random;
-import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * Hello world!
+ * 
+ * @author Andrea Ligios
+ * @version 1.0.0
+ * 
+ * FilenameSanitizer performs the sanitization of a filename in order to make it safe to be used for a file creation on every modern File System. 
+ * <p>
+ * Unallowed chars are replaced with a safe token (an underscore) according to every modern File System naming convention.
+ * <p>
+ * <b>Standard Sanitization</b> replaces:
+ * <ul>
+ * <li> the {@code NUL} (0) character
+ * <li> Control Codes between 1 and 31
+ * <li> {@code <} (less than)
+ * <li> {@code >} (greater than)
+ * <li> {@code :} (colon)
+ * <li> {@code "} (double quote)
+ * <li> {@code /} (forward slash)
+ * <li> {@code \} (backslash)
+ * <li> {@code |} (vertical bar or pipe)
+ * <li> {@code ?} (question mark)
+ * <li> {@code *} (asterisk)
+ * <li> leading and trailing spaces 
+ * </ul>
+ * 
+ * In addition, it prepends a safe token to files which correspond to one of the Windows reserved filenames (CON, PRN, AUX, NUL, COM1, COM2, COM3, COM4, COM5, COM6, COM7, COM8, COM9, LPT1, LPT2, LPT3, LPT4, LPT5, LPT6, LPT7, LPT8, and LPT9), with or without extension.
+ * <p>
+ * 
+ * <b>Safe Sanitization</b> also handles invalid inputs, specifically:
+ * <ul>
+ * <li> null file names, by generating a safe, unique filename
+ * <li> empty or whitespace-only file names, by generating a safe, unique filename 
+ * <li> file names too long (higher than 256 charaters, since on Windows the MAX_PATH is 260 characters and includes drive letter, colon, backslash and terminal NUL, like C:\file-256-chars-long<NUL>.
+ * </ul><p>
+ * 
+ * <b>Pretty Sanitization</b> behaves identically to a Safe Sanitization, but it also removes characters which (even if allowed) could be dangerous or annoying. Specifically:
+ * <ul>
+ * <li> leading hyphens (make tricky to perform "rm -filename" since - is used to express command options)
+ * <li> leading dots (make files semi-hidden on *NIX systems, and dangerous to be removed massively with "rm .*")
+ * <li> trailing dots (make Windows angry)
+ * </ul><p>
  *
+ * The logging is performed through SLF4J, which defaults to NOP (No OPeration) if no binding is be specified. 
+ *
+ * @see <a href="https://msdn.microsoft.com/en-us/library/windows/desktop/aa365247(v=vs.85).aspx">Naming Files, Paths, and Namespaces</a>
+ * @see <a href="https://www.dwheeler.com/essays/fixing-unix-linux-filenames.html">Fixing Unix/Linux/POSIX Filenames: Control Characters (such as Newline), Leading Dashes, and Other Problems</a>
+ * 
  */
 public class FilenameSanitizer {
 
 	private final static Logger LOG = LoggerFactory.getLogger(FilenameSanitizer.class);
+
+	final static int 			MAX_LENGTH 				= 256;  
+	final static char 			SANITIZATION_TOKEN 		= '_';	
+	final static String 		FILENAME_NULL 			= "Filename-parameter-was-null-at-time-";
+	final static String 		FILENAME_EMPTY 			= "Filename-parameter-was-empty-at-time-";
+	final static String 		EXTENSION_UNKNOWN 		= ".unknown";	
+	final static String 		RESERVED_CHARS 			= "<>:\"/\\|?*";
+	final static String[] 		RESERVED_NAMES			= {"CON", "PRN", "AUX", "NUL", "COM1", "COM2", 
+			                                               "COM3", "COM4", "COM5", "COM6", "COM7", "COM8", 
+			                                               "COM9", "LPT1", "LPT2", "LPT3", "LPT4", "LPT5", 
+			                                               "LPT6", "LPT7", "LPT8", "LPT9"};
 	
+
+	/**
+	 * Make the filename safe to be used on basically every modern File System. 
+	 * 
+	 * @param 	filename 	the file name to sanitize
+	 * @return 				the file name sanitized and trimmed 
+	 * @throws 				NullPointerException if filename is null
+	 * @throws 				IllegalArgumentException if filename is empty or whitespace only
+	 */
+	public static String sanitize(String filename) {
+		return sanitization(filename,false,false);
+	}
+
+	/**
+	 * Make the filename safe to be used on basically every modern File System. 
+	 * If a null, empty or whitespaces-only filename is passed, a safe filename is generated and no exception is thrown.  
+	 * 
+	 * @param 	filename 	the file name to sanitize
+	 * @return 				the file name sanitized and trimmed 
+	 */
 	public static String sanitizeSafely(String filename) {
+		return sanitization(filename,true,false);
+	}
+
+	/**
+	 * Make the filename safe to be used on basically every modern File System. 
+	 * If a null, empty or whitespaces-only filename is passed, a safe filename is generated and no exception is thrown.  
+	 * Trailing dots, leading dots and leading hyphens are stripped out in order to make the file OS-friendly.
+	 * 
+	 * 
+	 * @param 	filename 	the file name to sanitize
+	 * @return 				the file name sanitized, trimmed and prettified
+	 */
+	public static String sanitizePrettily(String filename) {
+		return sanitization(filename,true,true);
+	}
+	
+
+	
+	private static String sanitization(String input, boolean safely, boolean prettily) {
+		
+		String filename = (safely) 
+					? ((prettily) 
+						? makePretty(makeSafe(input)) 
+						: makeSafe(input)) 
+					: input;
+
+		throwErrorIfUnsafe(filename);
+
+		String output = handleRservedNames(handleReservedChars(filename));
+		
+		LOG.debug("\n INPUT : {}\nOUTPUT : {}\n" , input, output);
+		return output;
+	}
+
+	/** 
+	 * If a sanitized filename, extension or not, is equals to one of the Windows reserved names, prefix the file with a safe token (an underscore), making the filename legit.
+	 */
+	private static String handleRservedNames(String filename) {
+		String filenameWithoutExtension = stripExtensionIfAny(filename);
+
+		if (Stream.of(RESERVED_NAMES).anyMatch(s -> s.equalsIgnoreCase(filenameWithoutExtension))){
+			return SANITIZATION_TOKEN + filename;
+		}
+		
+		return filename;
+	}
+	
+	private static String stripExtensionIfAny(String filename) {
+		if (filename.indexOf('.')>0) {
+			return filename.substring(0,filename.lastIndexOf('.'));
+		}
+		return filename;
+	}
+	
+	private static String handleReservedChars(String filename) {
+		return filename.codePoints().map(i -> i > 31 ? i : SANITIZATION_TOKEN)
+							 .map(i -> RESERVED_CHARS
+									 	.codePoints()
+									 	.noneMatch(num -> Objects.equals(num, i)) 
+									 	? i 
+									 	: SANITIZATION_TOKEN)
+							 .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+							 .toString().trim();	
+	}
+	
+	private static String makeSafe(String filename) {
+		return handleMaxLength(handleEmpty(handleNull(filename)));
+	}
+	
+	private static String makePretty(String filename) {
+		String output = filename;
+
+		// Prevents filename ending in dot, which are dangerous on windows
+		while (output.length()>0 && (output.endsWith(".") || output.endsWith(" "))) {
+			output = output.substring(0,output.length()-1);
+		}
+
+		// Prevents filename starting with an hyphen which are dangerous in bash commands
+		while (output.length()>0 && (output.startsWith("-") || output.startsWith(".") || output.startsWith(" "))) {
+			output = output.substring(1,output.length());
+		}
+		
+		return handleEmpty(output.trim());
+	}
+
+	private static String handleNull(String filename) {
 		if (filename == null) {
 			LOG.warn("Filename was null, and a safe, unique filename has been generated");
 			return FILENAME_NULL + System.currentTimeMillis() + "-rnd-" + new Random(System.nanoTime()).nextLong() + EXTENSION_UNKNOWN;
 		}
+		return filename;
+	}
+	
+	private static String handleEmpty(String filename) {
 		if (filename.trim().isEmpty()) {
 			LOG.warn("Filename was empty, and a safe, unique filename has been generated"); 
 			return FILENAME_EMPTY + System.currentTimeMillis() + "-rnd-" + new Random(System.nanoTime()).nextLong() + EXTENSION_UNKNOWN;
 		}
+		return filename;
+	}
+	
+	private static String handleMaxLength(String filename) {
 		if (filename.length()>MAX_LENGTH) {
 			String shortenedFileName = filename.substring(0, MAX_LENGTH); 
-			LOG.warn("Filename proposed was too long (" + filename.length() + " chars against " + MAX_LENGTH + " of maximum length), and it has been shortened to:\n" + shortenedFileName);
-			return FilenameSanitizer.sanitize(shortenedFileName);
+			LOG.warn("Filename proposed was too long ({} chars against {} of maximum length), and it has been shortened to: {}",filename.length(), MAX_LENGTH, shortenedFileName);
+			return shortenedFileName;
 		}
-		
-		return FilenameSanitizer.sanitize(filename);
+		return filename;
 	}
 
-	public static String sanitize(String filename) throws NullPointerException, IllegalArgumentException {
-
+	private static void throwErrorIfUnsafe(String filename) {
 		if (filename.trim().isEmpty()) {
 			String error = "Input String has zero non-whitespace characters";
 			LOG.error(error);
@@ -46,34 +212,6 @@ public class FilenameSanitizer {
 			LOG.error(error);
 			throw new IllegalArgumentException(error);
 		}
-
-
-		IntStream chars = filename.codePoints();
-
-		String output = chars.map(i -> i > 31 ? i : SANITIZATION_TOKEN)
-							 .map(i -> reservedSpecialChars
-									 	.codePoints()
-									 	.noneMatch(num -> Objects.equals(num, i)) 
-									 	? i 
-									 	: SANITIZATION_TOKEN)
-							 .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
-							 .toString();
-
-		LOG.debug("\n INPUT : " + filename + "\n" + "OUTPUT : " + output);
-		return output;
-
 	}
-
-	
-	private final static int MAX_LENGTH = 256;  // MAX_PATH is 260 and includes drive, colon, backslash and the invisible terminating <NUL> char
-	private final static char SANITIZATION_TOKEN = '_';
-	
-	private final static String FILENAME_NULL 		= "Filename-parameter-was-null-at-time-";
-	private final static String FILENAME_EMPTY 		= "Filename-parameter-was-empty-at-time-";
-	private final static String EXTENSION_UNKNOWN 	= ".unknown";
-	
-	private final static String reservedSpecialChars = "<>:\"/\\|?*";
-	
-	
 
 }
--- src/test/java/com/andrealigios/legendaryutils/FilenameSanitizerTest.java
+++ src/test/java/com/andrealigios/legendaryutils/FilenameSanitizerTest.java
@@ -1,6 +1,11 @@
 package com.andrealigios.legendaryutils;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Collections;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Test;
 /**
@@ -8,26 +13,45 @@ import org.junit.jupiter.api.Test;
  */
 class FilenameSanitizerTest {
 
-	private final static String FILENAME_ENGLISH = "This is an English document.txt";
-	private final static String FILENAME_ITALIAN = "Questo è un documento Italiano.txt";
-	private final static String FILENAME_RUSSIAN = "Это российский документ.txt";
-	private final static String FILENAME_CHINESE = "这是一个中文文档.txt";
-	private final static String FILENAME_HINDI   = "यह एक हिंदी दस्तावेज़ है.txt";
-	private final static String FILENAME_ARAB    = "هذه وثيقة عربية.txt";
-	
+    // FILENAME WITH INTERNATIONALIZED INPUTS
     @Test
-    void givenProperMultilingualInput_WhenParsed_ThenOutputUnchanged() {
-    	assertEquals(FilenameSanitizer.sanitize(FILENAME_ENGLISH), FILENAME_ENGLISH);
-    	assertEquals(FilenameSanitizer.sanitize(FILENAME_ITALIAN), FILENAME_ITALIAN);
-    	assertEquals(FilenameSanitizer.sanitize(FILENAME_RUSSIAN), FILENAME_RUSSIAN);
-    	assertEquals(FilenameSanitizer.sanitize(FILENAME_CHINESE), FILENAME_CHINESE);
-    	assertEquals(FilenameSanitizer.sanitize(FILENAME_HINDI),   FILENAME_HINDI);
-    	assertEquals(FilenameSanitizer.sanitize(FILENAME_ARAB),    FILENAME_ARAB);
+    void givenProperMultilingualInput_whenSanitized_ThenOutputUnchanged() {
+    	
+        String FILENAME_ENGLISH = "This is an English document.txt";
+	   	String FILENAME_ITALIAN = "Questo è un documento Italiano.txt";
+	   	String FILENAME_RUSSIAN = "Это российский документ.txt";
+	   	String FILENAME_CHINESE = "这是一个中文文档.txt";
+	   	String FILENAME_HINDI   = "यह एक हिंदी दस्तावेज़ है.txt";
+	   	String FILENAME_ARAB    = "هذه وثيقة عربية.txt";
+    	
+	   	assertEquals(FilenameSanitizer.sanitize(FILENAME_ENGLISH), FILENAME_ENGLISH);
+	   	assertEquals(FilenameSanitizer.sanitize(FILENAME_ITALIAN), FILENAME_ITALIAN);
+	   	assertEquals(FilenameSanitizer.sanitize(FILENAME_RUSSIAN), FILENAME_RUSSIAN);
+	   	assertEquals(FilenameSanitizer.sanitize(FILENAME_CHINESE), FILENAME_CHINESE);
+	   	assertEquals(FilenameSanitizer.sanitize(FILENAME_HINDI),   FILENAME_HINDI);
+	   	assertEquals(FilenameSanitizer.sanitize(FILENAME_ARAB),    FILENAME_ARAB);
     }
-    
+
+    // FILENAME WITH SURROGATE PAIR 
     @Test
-    void givenSpecialWindowsCharacters_WhenParsed_ThenReplacedWithUnderscore() {
-    	assertEquals(FilenameSanitizer.sanitize("A filename with a [<] less-than sign"), 
+    void givenSurrogatePair_whenSanitized_ThenOutputUnchanged() {
+    	String FILENAME_WITH_SURROGATE_PAIR    = "This is a document with a Surrogate Pair (𐐂) in it.txt";
+    	assertEquals(FilenameSanitizer.sanitize(FILENAME_WITH_SURROGATE_PAIR), FILENAME_WITH_SURROGATE_PAIR);
+    }
+
+    // FILENAME WITH CONTROL CODES
+    @Test
+    void givenControlCodes_whenSanitized_ThenReplacedWithUnderscore() {
+    	for (int x=0;x<32;x++) {
+    		assertEquals(FilenameSanitizer.sanitize("A filename with control code n."+x+"["+(char)x+"]"), 
+    												"A filename with control code n."+x+"[_]");
+    	}
+    }
+
+    // FILENAME WITH RESERVED CHARS
+    @Test
+    void givenSpecialCharacters_whenSanitized_ThenReplacedWithUnderscore() {
+        assertEquals(FilenameSanitizer.sanitize("A filename with a [<] less-than sign"), 
     										    "A filename with a [_] less-than sign");
     	
     	assertEquals(FilenameSanitizer.sanitize("A filename with a [>] more-than sign"), 
@@ -45,8 +69,8 @@ class FilenameSanitizerTest {
     	assertEquals(FilenameSanitizer.sanitize("A filename with a [\\] backslash"), 
     											"A filename with a [_] backslash");
     	
-    	assertEquals(FilenameSanitizer.sanitize("A filename with a [|] pipe"), 
-												"A filename with a [_] pipe");
+    	assertEquals(FilenameSanitizer.sanitize("A filename with a [|] vertical bar"), 
+												"A filename with a [_] vertical bar");
     	
     	assertEquals(FilenameSanitizer.sanitize("A filename with a [?] question mark"), 
 												"A filename with a [_] question mark");
@@ -55,5 +79,93 @@ class FilenameSanitizerTest {
 												"A filename with a [_] asterisk");
     }
 
+    // FILENAME WITH CONTROL CODES
+    @Test
+    void givenReservedNames_whenSanitized_ThenPrefixedWithUnderscore() {
+    	
+    	Stream.of(FilenameSanitizer.RESERVED_NAMES).forEach(reservedName -> {
+    		
+    		assertEquals(FilenameSanitizer.sanitize(reservedName), 
+    				                          "_" + reservedName);
+    		
+    		assertEquals(FilenameSanitizer.sanitize(reservedName + ".txt"), 
+    				                          "_" + reservedName + ".txt");
+    	});
+    }
+    
+	// FILENAME TOO LONG
+    @Test
+    void givenFilenameTooLong_whenSanitized_ThenIllegalArgumentExceptionIsThrown() {
+    	String stringOf16Chars = "1234567890abcdef";
+    	String maximumFileNameOf256Chars 	= String.join("", Collections.nCopies(16, stringOf16Chars));
+    	String excessiveFileNameOf272Chars 	= String.join("", Collections.nCopies(17, stringOf16Chars));    	
+    	
+    	assertEquals(FilenameSanitizer.sanitize(maximumFileNameOf256Chars), maximumFileNameOf256Chars);
+    	assertThrows(IllegalArgumentException.class, () -> FilenameSanitizer.sanitize(excessiveFileNameOf272Chars));
+    }
+    @Test
+    void givenFilenameTooLong_whenSanitizedSafely_ThenFilenameShortened() {
+    	String stringOf16Chars = "1234567890abcdef";
+    	String maximumFileNameOf256Chars 	= String.join("", Collections.nCopies(16, stringOf16Chars));
+    	String excessiveFileNameOf272Chars 	= String.join("", Collections.nCopies(17, stringOf16Chars));    	
+
+    	assertEquals(FilenameSanitizer.sanitizeSafely(excessiveFileNameOf272Chars), maximumFileNameOf256Chars);
+    }    
+
+    // FILENAME EMPTY or WHITESPACES-ONLY
+    @Test
+    void givenEmptyOrWhitespaces_whenSanitized_ThenIllegalArgumentExceptionIsThrown() {
+    	assertThrows(IllegalArgumentException.class, () -> FilenameSanitizer.sanitize(""));
+    	assertThrows(IllegalArgumentException.class, () -> FilenameSanitizer.sanitize(" "));
+    }    
+    @Test
+    void givenEmptyOrWhitespaces_whenSanitizedSafely_ThenPlaceholderIsGenerated() {
+    	assertTrue(FilenameSanitizer.sanitizeSafely("").startsWith(FilenameSanitizer.FILENAME_EMPTY));
+    	assertTrue(FilenameSanitizer.sanitizeSafely(" ").startsWith(FilenameSanitizer.FILENAME_EMPTY));
+    }
+
+    // FILENAME NULL
+    @Test
+    void givenNull_whenSanitized_ThenNullPonterExceptionIsThrown() {
+    	assertThrows(NullPointerException.class, () -> FilenameSanitizer.sanitize(null));
+    }    
+    @Test
+    void givenNull_whenSanitizedSafely_ThenPlaceholderIsGenerated() {
+    	assertTrue(FilenameSanitizer.sanitizeSafely(null).startsWith(FilenameSanitizer.FILENAME_NULL));    	
+    }
+
+    // FILENAME WITH TRAILING DOTS / WHITESPACES
+    @Test
+    void givenTrailingDots_whenSanitizedPrettily_ThenStrippedOut() {    	
+    	String filenameEndingIndot 				= "A filename ending in dot.";    	
+    	String filenameEndingIndots1 			= "A filename ending in dot....";
+    	String filenameEndingIndots2 			= " - A filename ending in dot .. .. ";
+    	String expected 						= "A filename ending in dot";
+    	
+    	assertEquals(FilenameSanitizer.sanitizePrettily(filenameEndingIndot), expected);
+    	assertEquals(FilenameSanitizer.sanitizePrettily(filenameEndingIndots1), expected);
+    	assertEquals(FilenameSanitizer.sanitizePrettily(filenameEndingIndots2), expected);
+    	    	
+    	String dotsOnly = "....";
+    	assertTrue(FilenameSanitizer.sanitizePrettily(dotsOnly).startsWith(FilenameSanitizer.FILENAME_EMPTY));
+    }
+
+    // FILENAME WITH LEADING HYPHENS / WHITESPACES 
+    @Test
+    void givenLeadingHyphens_whenSanitizedPrettily_ThenStrippedOut() {    	
+    	String filenameStartingWithHyphen		= "-A filename starting with hypen";
+    	String filenameStartingWithHyphens1 	= "----A filename starting with hypen";
+    	String filenameStartingWithHyphens2 	= " --. -- A filename starting with hypen . ";
+    	String filenameStartingWithHyphens3 	= ".---- A filename starting with hypen.";
+    	String expected 						= "A filename starting with hypen";
+    	    	
+    	assertEquals(FilenameSanitizer.sanitizePrettily(filenameStartingWithHyphen), expected);
+    	assertEquals(FilenameSanitizer.sanitizePrettily(filenameStartingWithHyphens1), expected);
+    	assertEquals(FilenameSanitizer.sanitizePrettily(filenameStartingWithHyphens2), expected);
+    	assertEquals(FilenameSanitizer.sanitizePrettily(filenameStartingWithHyphens3), expected);
+    	
+    	String hyphensOnly = "----";
+    	assertTrue(FilenameSanitizer.sanitizePrettily(hyphensOnly).startsWith(FilenameSanitizer.FILENAME_EMPTY));
+    }    
     
 }
